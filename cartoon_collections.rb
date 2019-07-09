0743a186d7d594c51727950ebdfe9dcbd891f4bf
Veronica Lopez <nicaa0695@gmail.com>
2019-07-09 01:02:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,19 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index { |dwarf, index| puts "{index +1}. {dwarf}"}
-end
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index|
+    puts "#index + 1}. #dwarf}"
+ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |planeteer | planeteer.capitalize + "!" }
 end
 
-def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? do | call |
+def long_planeteer_calls(calls)
+  calls.any? do |call|
     call.length > 4
 end
 
-def find_the_cheese(snacks)
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.find do | item |
-    cheesy_types.include?(item)
+  foods.find do |food|
+    cheese_types.include?(food)
 end
